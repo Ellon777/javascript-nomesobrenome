8a75df819cd78188446d475ad04665a12006b0cf
Elias <xgamerblackwinter@gmail.com>
2026-03-16 19:14:37 -0300

    funções adicionais, limpar, juntar, ajustar foco

--- devweb2/js/utilidades.js
+++ devweb2/js/utilidades.js
@@ -1,16 +1,41 @@
 function juntarNome() {
-    let      nome = document.getElementById("nome").value; 
+
+    let nome = document.getElementById("nome").value; 
     let sobrenome = document.getElementById("sobrenome").value;
-
-    // 1. Concatenação de strings normal.
     let nomecompleto = nome + ' ' + sobrenome;
-
-    // 2. Uso de interpolação strings (template strings).
     let nomecompleto1 = `<i>${nome} <br>${sobrenome}</i>`;
 
-    // 3. Injetar texto da variável nomecompleto na tag <p> cujo id é "nomecompleto".
     document.getElementById("nomecompleto").innerText = nomecompleto;
-
-    // 4. Injetar texto da variável nomecompleto1 na tag <p> cujo id é "nomecompleto1".
     document.getElementById("nomecompleto1").innerHTML = nomecompleto1;
+}
+function escreverNome() {
+    let nome = document.getElementById("nome").value;
+    let sobrenome = document.getElementById("sobrenome").value;
+
+    let css = '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">';
+    let resultado = "<strong>" + nome + " " + sobrenome + "</strong><br>";
+
+    document.writeln(css); 
+    document.writeln(resultado);
+    document.writeln("<button id='voltar' onclick='location.reload()'> " +
+                     "<i class='fa-solid fa-angle-left'></i> Voltar </button>");
+}
+function mostrarLog(n,s) {
+    console.log("Nome: " + n);
+    console.log("Sobrenome: " + s);
+    console.log("Nome Completo: " + n + " " + s);
+}
+function ajustarFoco() {
+    document.getElementById("nome").focus();
+}
+function limparDados() {
+
+    document.getElementById("nomes").reset();
+    document.getElementById("nomecompleto").innerText = "";
+    document.getElementById("nomecompleto1").innerHTML = "";
+
+    ajustarFoco();
+}
+function alerta() {
+    window.alert("Enquanto não fechar esta janela, \nnão será mostrado o nome completo.");
 }
